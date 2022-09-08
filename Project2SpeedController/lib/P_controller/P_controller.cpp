1e51d3464a69c9d04ff269c0ae9527c35a6b9ea1
Andy <amadden114@gmail.com>
2022-09-08 15:59:47 +0000

    added P_control class constructor

--- Project2SpeedController/lib/P_controller/P_controller.cpp
+++ Project2SpeedController/lib/P_controller/P_controller.cpp
@@ -1,4 +1,8 @@
 #include <P_controller.h>
+P_controller::P_controller(double K_p) {
+  k_p=K_p;
+}
+
 double P_controller::update(double ref, double actual){
   return k_p*(ref-actual);
 }
--- Project2SpeedController/lib/P_controller/P_controller.h
+++ Project2SpeedController/lib/P_controller/P_controller.h
@@ -2,5 +2,6 @@ class P_controller{
   private:
   double k_p=1000;
   public:
+    P_controller(double K_p);  
     double update(double ref, double actual);
 };
