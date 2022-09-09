30103bea01bbf4421016de830726a906977829aa
arnthorg <arnthorgisla@gmail.com>
2022-09-09 20:04:05 +0000

    timer was running at 100ms instead of 10ms because I was being clever and trying not to use floats
    managed to get it very unstable like this
    also cleaning up a bit

--- Project2SpeedController/src/main.cpp
+++ Project2SpeedController/src/main.cpp
@@ -8,7 +8,8 @@
 #include <P_controller.h>
 #include "PWM2.h"
 
-#define DELTA_T (int32_t)100 // 1/0.01 = 100
+#define DELTA_T (int32_t)10 
+#define INV_DELTA_T (int32_t)100 // 1/0.01 = 100
 #define PPR (int32_t)(7*2*2) // 7 pulses per phase and triggering on falling as well
 #define GEAR_REDUCTION (int32_t)101
 
@@ -52,7 +53,6 @@ int main()
 {
   USART_Init(MYUBRR);
   timer.init(DELTA_T);
-  led.init();
   pwm.init();
   pwm.set(0);
   
@@ -60,26 +60,21 @@ int main()
   
   set_interrupt_d1();
   set_interrupt_d2();
-  float kp = 255.0/80.0*1.1;
+  //float kp = 255.0/80.0*1.2;
   uint16_t duty = 0;
-  int16_t set_point = 60;
+  int16_t set_point = 10;
+  float kp = 255.0*1.2/float(set_point);
   float out;
 
   while (true) {
-    _delay_ms(100);
-    delta_counts = delta_counts/10;
-    rpm = delta_counts * 60 / PPR * DELTA_T / GEAR_REDUCTION;  // accurate but delta_counts is 10x the value
+    _delay_ms(100); // if duty is set more often it gets stuck at 50% duty
+    delta_counts = delta_counts/10; //  delta_counts is 10x the value for some reason
+    rpm = delta_counts * 60 / PPR * INV_DELTA_T / GEAR_REDUCTION;  // accurate
 
     duty = (int16_t)(kp * (float)(set_point - rpm)); // RPM of output shaft, not rpm of input shaft!!
     if(duty>255)duty=255;
     if(duty<0) duty=0;
     pwm.set((uint8_t)duty&0xFF);
-    //print_i(duty);
-
-
-
-    
-    //print_i(rpm);
   }
 
   
