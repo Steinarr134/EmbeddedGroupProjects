40e07063380dc312446ed42eb7b96dc0efaf4212
Arnþór G <arnthorgisla@gmail.com>
2022-09-23 20:40:32 +0000

    first iteration of controller and PI_controller,
    also copying main from P2

---
+++ Project3ControllerStateMachine/lib/PI_controller/controller.h
@@ -0,0 +1,43 @@
+#include <stdint.h>
+
+class controller {
+    public:
+    //controller(double K_p, uint8_t max_rpm, uint8_t max_pwm, double K_i=0, double K_d=0); // why even use polymorphism
+    //controller(double K_p, uint8_t max_rpm, uint8_t max_pwm);
+    virtual uint8_t update(double set_point, double actual);   
+    protected:
+    double k_p;
+    double k_i;
+    double k_d;
+    int16_t max_rpm_;
+    uint8_t max_pwm_;
+    int32_t integral_;
+    double delta_t_;
+};
+
+class PI_controller : controller {
+    PI_controller(double K_p, double K_i, uint16_t max_rpm, uint8_t max_pwm, double delta_t);
+    uint8_t update(double set_point, double actual);
+};
+
+
+    PI_controller::PI_controller(double K_p, double K_i, uint16_t max_rpm, uint8_t max_pwm, double delta_t) {
+        k_p = K_p;
+        k_i = K_i;
+        max_rpm_ = max_rpm;
+        max_pwm_ = max_pwm;
+        integral_ = 0;
+        delta_t = delta_t;
+
+    }
+    uint8_t PI_controller::update(double set_point, double actual) {
+        double e = set_point - actual;
+        integral_ += e * delta_t_;
+        double u=k_p*(e)/max_rpm_ + k_i * integral_;
+        u = u*max_pwm_;
+        if (u>255) u=255;
+        if (u<0) u=0;
+
+        return (uint8_t)u;
+        
+    }
--- Project3ControllerStateMachine/src/main.cpp
+++ Project3ControllerStateMachine/src/main.cpp
@@ -1,9 +1,95 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <digital_out.h>
+#include <timer_msec.h>
+#include <encoder_simple.h>
+#include <hackySerial.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <PI_controller.h>
+#include "PWM2.h"
+#include <encoder_interrupt.h>
+#include "speedometer.h"
+
+#define DELTA_T (int32_t)5
+#define INV_DELTA_T (int32_t)1.0 / ((float)DELTA_T / 1000.0)
+#define PPR (int32_t)(7 * 2 * 2) // 7 pulses per phase and triggering on falling as well
+#define GEAR_REDUCTION (int32_t)101
+#define MAX_PWM 255
+#define MAX_RPM 15000
+
+uint16_t duty = 0;
+int16_t set_point = 5000;
+float kp = 6; // gain
+float ki = 1;
+
+Encoder_interrupt encoder;
+Digital_out led(5);
+Timer_msec timer;
+PWM2 pwm;
+PI_controller controller(kp, ki, MAX_RPM, MAX_PWM);
+volatile int32_t counter = 0;
+volatile int32_t delta_counts = 0;
+volatile bool flag = false;
+volatile uint16_t time = 0;
+int32_t rpm = 0; // initialize just cause
+unsigned int count = 0;
+int32_t dc;
+
+int main()
+{
+  USART_Init(0); // 1megabaud
+  // USART_Init(MYUBRR); // 9600
+  timer.init(DELTA_T);
+  pwm.init();
+  pwm.set(0);
+  encoder.init();
+
+  while (true)
+  {
+    if (flag)
+    { // if there's a new measurement available
+      dc = delta_counts;
+      if (dc < 0)
+      {
+        dc = -dc;
+      }
+      rpm = dc * (int32_t)60 * INV_DELTA_T / PPR;
+      duty = (int16_t)controller.update(set_point, rpm); // RPM of input shaft, not rpm of output shaft!!
+      print_3_numbers(set_point, rpm, duty);
+      pwm.set(duty);
+      flag = false;
+      count++;
+    }
+
+    if (count > 1000)
+    { // to vary the set point
+      count = 0;
+      if (set_point == 5000)
+      {
+        set_point = 10000;
+      }
+      else
+      {
+        set_point = 5000;
+      }
+    }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+ISR(INT0_vect)
+{
+  encoder.pin1();
+}
+
+ISR(INT1_vect)
+{
+  encoder.pin2();
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  delta_counts = encoder.position();
+  encoder.reset();
+  flag = true;
 }
