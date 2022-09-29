5cf80086df9433a13d5ca65d7c849b587debbc13
Arnþór G <arnthorgisla@gmail.com>
2022-09-29 16:51:47 +0000

    trying to mash it together, million errors

--- Project3ControllerStateMachine/src/main.cpp
+++ Project3ControllerStateMachine/src/main.cpp
@@ -5,7 +5,6 @@
 #include "timer_usec.h"
 #include "timer0_msec.h"
 #include "encoder_simple.h"
-#include "hackySerial.h"
 
 #include "controller.h" // todo: move PI controller to separate file 
 #include "encoder_interrupt.h"
@@ -18,6 +17,14 @@
 #include "state_operation.h"
 #include "state_initialization.h"
 
+#include "hackySerial.h"
+#include "state.h"
+// #include "Arduino.h"
+#define FOSC 16000000 // Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC / 16 / BAUD - 1
+
+#define buffer_size 10
 
 #define DELTA_T (uint8_t)5
 #define INV_DELTA_T (int32_t)(1.0 / ((float)DELTA_T / 1000.0))
@@ -27,6 +34,161 @@
 #define MAX_PWM 255
 #define MAX_RPM 15000
 #define TIMER_RESOLUTION (double)500e-9
+
+unsigned char b[buffer_size]; // buffer for incoming stuff
+unsigned char toolongmsg[] = {'t', 'o', 'o', ' ', 'l', 'o', 'n', 'g'};
+
+// for creating a print statement:
+// def p(s):
+//     print("print((unsigned char){'" + "', '".join(s) + "'}, " + str(len(s)) + ");")
+// def p2(s):
+//      for c in s: 
+//              print("print_one('" + c + "');")
+//      print("println();")
+
+
+uint8_t b_i; // index, how many characters have been received unsigned char b_t; // first character
+
+Parsed parse()
+{
+    // b should now be on the form p=1.34\0\0\0\0
+    Parsed ret;
+    ret.what = b[0];
+
+    double value = 0;
+    int tens = 1;
+    bool decimal_done = false;
+    for (int i = 1; i < buffer_size; i++)
+    {
+        if (b[i] == '.' || ('0' <= b[i] && b[i] <= '9'))
+        {
+            if (b[i] == '.')
+            {
+                decimal_done = true;
+                tens = 10;
+                continue;
+            }
+            if (b[i] == 0)
+            {
+                ret.val = value;
+                return ret;
+            }
+            if (!decimal_done)
+            {
+                value *= tens;
+                value += b[i] - '0';
+                tens *= 10;
+            }
+            else
+            {
+                value += (double)(b[i] - '0') / tens;
+                tens *= 10;
+            }
+        }
+    }
+    ret.val = value;
+    return ret;
+}
+
+void reset_buffer()
+{
+    for (int i = 0; i< buffer_size; i++)
+    {
+        b[i] = 0;
+    }
+    b_i = 0;
+}
+
+Context *context;
+
+class Init : public State
+    {
+    public:
+        void on_do() override {}
+        void on_entry() override {
+            print_one('I');
+            print_one('n');
+            print_one('i');
+            print_one('t');
+            println();
+        }
+        void on_exit() override{}
+        void set(Parsed p) override {}
+        void cmd(unsigned char C) override {}
+    };
+class PreOp : public State
+    {
+    public:
+        void on_do() override {}
+        void on_entry() override {
+            print_one('P');
+            print_one('r');
+            print_one('e');
+            print_one('O');
+            print_one('p');
+            println();
+        }
+        void on_exit() override {}
+        void set(Parsed p) override {}
+        void cmd(unsigned char C) override {}
+    };
+
+class Op : public State
+    {
+    public:
+        void on_do() override {
+    if (flag)
+    { // if there's a new measurement available
+      dc = delta_counts; 
+      //convert dc to us, each count is .5µs or .5µs/count => .5µs/count * counts = µs
+      double t = TIMER_RESOLUTION * (double)dc; 
+      if(timer_u.overflow()) {
+        rpm = 0;
+      }
+      else {
+        rpm = (int32_t)(double)SECONDS_TO_MINUTE/((double)PPR*t);
+        if(!encoder.forward()){
+          rpm = -rpm;
+        }
+        if(rpm > MAX_RPM || rpm < -MAX_RPM) { // at startup rpm can get to some insane number that hacky serial can't even comprehend 
+          rpm = 0;
+        }
+
+      }
+      duty = (int16_t)controller.update(set_point, rpm); // RPM of input shaft, not rpm of output shaft!!
+      print_3_numbers(set_point, rpm, duty);
+      motor_controller.update(duty);
+      flag = false;
+      count++;
+    }
+
+        }
+        void on_entry() override {
+            print_one('O');
+            print_one('p');
+            println();
+        }
+        void on_exit() override {}
+        void set(Parsed p) override {}
+        void cmd(unsigned char C) override {}
+    };
+
+class Stop : public State
+    {
+    public:
+        void on_do() override {}
+        void on_entry() override {
+            print_one('S');
+            print_one('t');
+            print_one('o');
+            print_one('p');
+            println();
+        }
+        void on_exit() override {}
+        void set(Parsed p) override {}
+        void cmd(unsigned char C) override {}
+    };
+
 
 int16_t duty = 0;
 int16_t set_point = 5000;
@@ -56,47 +218,66 @@ int main()
   timer_u.init();
   timer_msec.init(DELTA_T);
   encoder.init();
-  DDRC |= (1<<0);
-  while (true)
-  {
-    if (flag)
-    { // if there's a new measurement available
-      dc = delta_counts; 
-      //convert dc to us, each count is .5µs or .5µs/count => .5µs/count * counts = µs
-      double t = TIMER_RESOLUTION * (double)dc; 
-      if(timer_u.overflow()) {
-        rpm = 0;
-      }
-      else {
-        rpm = (int32_t)(double)SECONDS_TO_MINUTE/((double)PPR*t);
-        if(!encoder.forward()){
-          rpm = -rpm;
-        }
-        if(rpm > MAX_RPM || rpm < -MAX_RPM) { // at startup rpm can get to some insane number that hacky serial can't even comprehend 
-          rpm = 0;
-        }
+  context = new Context(new Init);
+  while (true) {
 
-      }
-      duty = (int16_t)controller.update(set_point, rpm); // RPM of input shaft, not rpm of output shaft!!
-      print_3_numbers(set_point, rpm, duty);
-      motor_controller.update(duty);
-      flag = false;
-      count++;
-    }
-    //set_point = 0; // try setting set point to 0 and turn the motor
-    //continue;
-    if (count > 1000)
-    { // to vary the set point
-      count = 0;
-      if (set_point == 500)
-      {
-        set_point = 5000;
-      }
-      else
-      {
-        set_point = 500;
-      }
-    }
+        if (USART_receive_ready())
+        {
+            unsigned char inn = USART_Receive();
+            // print_i_ln(inn);
+            USART_Transmit(inn);
+            if (inn == 8)
+            {
+                // backspace
+                if (b_i > 0)
+                    b_i--;
+                continue;
+            }
+            if (b_i >= buffer_size && inn != '\n'){
+                println(toolongmsg, sizeof(toolongmsg));
+                reset_buffer();
+                continue;
+            }
+            else
+            {
+                if (inn == '\n')
+                {
+                    if ('A' <= b[0] && b[0] <= 'Z')
+                    {
+                        // send command to current state
+                        if (b[0] == 'I')
+                            context->transition_to(new Init);
+                        else if (b[0] == 'P')
+                            context->transition_to(new PreOp);
+                        else if (b[0] == 'O')
+                            context->transition_to(new Op);
+                        else if (b[0] == 'S')
+                            context->transition_to(new Stop);
+                        else
+                        {
+                            context->cmd(b[0]);
+                        }
+                        
+                    }
+                    else if ('a' <= b[0] && b[0] <= 'z')
+                    {
+                        // send parsed to current state
+                        context->set(parse());
+                    }
+                    reset_buffer();
+                }
+                else
+                {
+                    b[b_i++] = inn;
+                }
+                
+            }
+        }
+ 
+ 
+ 
+    //state operational
+
   }
 }
 
